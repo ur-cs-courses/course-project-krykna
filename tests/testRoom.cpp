4afd6c78d24428a1c671a0404b754040b0658485
Kate <143840870+KathrynMew@users.noreply.github.com>
2023-11-02 12:19:58 -0400

    Updated testRoom.cpp
    Realized I need to add some functionality for viewing a room that was created by default

--- tests/testRoom.cpp
+++ tests/testRoom.cpp
@@ -4,9 +4,11 @@
 
 int main() {
     Room* myroom = new Room("Conference", "dirty", "small", "5");
-
-    TEST_CASE("Room Case Testing") {
+    TEST_CASE("Basic data fetching methods test") {
         const std::string expected_string = "Room Name: Conference\nRoom Status: Dirty\nRoom Size: Small\nEstimated Time to Clean: 5 minutes";
+        CHECK(myroom->get_estimated_clean_time() == 5);
+        CHECK(myroom->get_room_size() == Size::small);
+        CHECK(myroom->get_room_status() == Status::dirty);
         CHECK(myroom->get_room_data() == expected_string);
     }
 }
