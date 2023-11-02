3c81f1fe5a86e4be74ff83660c36bf1de4b8e1fc
Kate <143840870+KathrynMew@users.noreply.github.com>
2023-11-02 17:38:44 -0400

    Small update to testRoom.cpp

--- tests/testRoom.cpp
+++ tests/testRoom.cpp
@@ -3,17 +3,17 @@
 #include <iostream>
 
 int main() {
-    Room* myroom = new Room("Conference", "dirty", "small", "5");
+    Room myroom("Conference", "dirty", "small", "5");
     TEST_CASE("Basic data fetching methods test") {
-        const std::string expected_string = "Room Name: Conference\nRoom Status: Dirty\nRoom Size: Small\nEstimated Time to Clean: 5 minutes";
-        CHECK(myroom->get_estimated_clean_time() == 5);
-        CHECK(myroom->get_room_size() == Size::small);
-        CHECK(myroom->get_room_status() == Status::dirty);
-        CHECK(myroom->get_room_data() == expected_string);
+        const std::string expected_string = "Room Name:\tConference\nRoom Status:\tDirty\nRoom Size:\tSmall\nEstimated Time to Clean: 5 minutes";
+        CHECK(myroom.get_estimated_clean_time() == 5);
+        CHECK(myroom.get_room_size() == Size::small);
+        CHECK(myroom.get_room_status() == Status::dirty);
+        CHECK(myroom.get_room_data() == expected_string);
     }
 
-    Room* empty = new Room();
+    Room empty;
     TEST_CASE("Testing empty room") {
-        CHECK(empty->get_room_data() == "Room Name: TBD\nRoom Status: TBD\nRoom Size: TBD\nEstimated Time to Clean: 0 minutes");
+        CHECK(empty.get_room_data() == "Room Name:\tTBD\nRoom Status:\tTBD\nRoom Size:\tTBD\nEstimated Time to Clean: 0 minutes");
     }
 }
