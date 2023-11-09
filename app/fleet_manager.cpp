6a5915d4cbd3a90d882a7a82fbad177f545352c6
Waleed Said <waleedsaid543@gmail.com>
2023-11-09 12:05:14 -0500

    Command Line Driver Status Retriever

---
+++ app/fleet_manager.cpp
@@ -0,0 +1,19 @@
+#include <iostream> // Include the library that contains the standard input/output stream objects
+#include <string> // Including the string library
+#include <fstream> // Including file stream library for CSV files
+
+
+using namespace std; // Use the standard namespace
+
+int main() {
+    //Introduction Message
+    cout << "Welcome to your Robot Fleet Management System! "; 
+    
+    // Variables for file handling
+    ifstream robotCSV, roomCSV;
+    string robotCSV, roomCSV;
+
+    
+
+    return 0; // End of the program
+}
