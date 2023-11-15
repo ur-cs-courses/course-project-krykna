a96f6d19cf67b6a0d580bd7f25bd31846b15f031
KathrynMew <kathrynmew@gmail.com>
2023-11-14 21:55:50 -0500

    Added Bot testing of assigning a room and going home

--- tests/robot_class_test.cpp
+++ tests/robot_class_test.cpp
@@ -1,15 +1,21 @@
-#include <libRobot/Robot.hpp>
-
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "libRobot/Robot.hpp"
 
-TEST_CASE("Independent Class: Robot Class") {
+TEST_CASE("Robot Class") {
     Robot robot_failed_1("Bubbles", "free", "large", "mop");
     Robot robot_passed_2("Bubbles", "Free", "Large", "Mop");
     std::string expected_robot = "Robot ID:\tBubbles\nStatus:\tFree\nSize:\tLarge\nType:\tMop";
     CHECK(robot_failed_1.to_string() == expected_robot);
     CHECK(robot_passed_2.to_string() == expected_robot);
+
+    Robot testRobot("SpongeBot", "Free", "Small", "Scrub");
+    SECTION("Robot is successfully assigned to a room + Sent back home") {
+        testRobot.set_room("Cruddy Crawfish");
+        CHECK(testRobot.get_room() == "Cruddy Crawfish");
+        testRobot.go_home();
+        CHECK(testRobot.get_room() == "");
+    }
 }
 
 // Comment
