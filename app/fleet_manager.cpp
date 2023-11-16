94e79322c9becddd927ab50c4c8abbfa505e5181
Waleed Said <waleedsaid543@gmail.com>
2023-11-15 22:52:54 -0500

    cli

--- app/fleet_manager.cpp
+++ app/fleet_manager.cpp
@@ -16,46 +16,55 @@ int main() {
     
     // Main loop for program to run
     while (true) {
-        cout << "Please type a command (Add Robot, Add Room, Get Overall Room Status, Get Overall Robot Status) or type Exit to quit:";
+        cout << "Please type a command (1: Add Robot, 2: Add Room, 3: Get Overall Room Status, 4: Get Overall Robot Status) or type 0 to Exit:";
         getline(cin, input); // Get the whole line as a command
 
+        int command = stoi(input);
+
         // Check the user input and respond accordingly
-        if (input == "Add Robot") {
-            string robotId, robotStatus, robotSize, robotType;
-            cout << "Enter: [Robot ID] [robotStatus] [robotSize] [robotType] (in this order):" << endl;
-            getline(cin, input); // Expecting user to input size and type in "size type" format
-            stringstream ss(input);
-            ss >> robotId >> robotStatus>> robotSize >> robotType; // Split the input into robot id, status, size, and 
-            
-            Robot newRobot(robotId, robotStatus, robotSize, robotType);
-            cout << "Robot Details:\n" << newRobot.to_string() << endl;
+        switch (command) {
+            case 1: {
+                string robotId, robotStatus, robotSize, robotType;
+                cout << "Enter: [Robot ID] [robotStatus] [robotSize] [robotType] (in this order):" << endl;
+                getline(cin, input); // Expecting user to input size and type in "size type" format
+                stringstream ss(input);
+                ss >> robotId >> robotStatus>> robotSize >> robotType; // Split the input into robot id, status, size, and 
+                
+                Robot newRobot(robotId, robotStatus, robotSize, robotType);
+                cout << "Robot Details:\n" << newRobot.to_string() << endl;
 
-            cout << "New Robot has been added to Robot CSV file! (NOT INTEGRATED YET)" << endl;
-        } else if (input == "Add Room") {
-            string roomName, roomStatus, roomSize, estimatedTime;
-            cout << "Enter: [Room Name] [Room Status] [Room Size] [Estimated Time] (in this order):" << endl;
-            getline(cin, input); // Expecting user to input details in "name status size time" format
-            stringstream ss(input);
-            ss >> roomName >> roomStatus >> roomSize >> estimatedTime;
+                cout << "New Robot has been added to Robot CSV file! (NOT INTEGRATED YET)" << endl;
+                break;
 
-            Room newRoom(roomName, roomStatus, roomSize, estimatedTime);
-            cout << "Room Details:\n" << newRoom.to_string() << endl;
+            } case 2: {
+                string roomName, roomStatus, roomSize, estimatedTime;
+                cout << "Enter: [Room Name] [Room Status] [Room Size] [Estimated Time] (in this order):" << endl;
+                getline(cin, input); // Expecting user to input details in "name status size time" format
+                stringstream ss(input);
+                ss >> roomName >> roomStatus >> roomSize >> estimatedTime;
 
-            cout << "New Room has been added to Room CSV file! (NOT INTEGRATED YET)" << endl;
-        } else if (input == "Get Overall Room Status") {
-            cout << "The backend will grab the to_string() from the Room class and use file line reading functionality to print out the updated contents of the entire Room CSV file " << endl;
-        } else if (input == "Get Overall Robot Status") {
-            cout << "The backend will grab the to_string() from the Robot class and use file line reading functionality to print out the updated contents of the entire Robot CSV file " << endl;
-        } else if (input == "Exit") {
-            break; // Exit the while loop and end the program
-        } else {
-            cout << "Invalid command. Please try again." << endl;
+                Room newRoom(roomName, roomStatus, roomSize, estimatedTime);
+                cout << "Room Details:\n" << newRoom.to_string() << endl;
+
+                cout << "New Room has been added to Room CSV file! (NOT INTEGRATED YET)" << endl;
+                break;
+
+            } case 3: {
+                cout << "The backend will grab the to_string() from the Room class and use file line reading functionality to print out the updated contents of the entire Room CSV file " << endl;
+                break;
+            } case 4: {
+                cout << "The backend will grab the to_string() from the Robot class and use file line reading functionality to print out the updated contents of the entire Robot CSV file " << endl;
+                break;
+            } case 0: {
+                cout << "Exiting Robot Fleet Management System." << endl;
+                return 0; // Exit the while loop and end the program
+            } default: {
+                cout << "Invalid command. Please try again." << endl;
+                break;
+            }
         }
-
+        
     }
-
-    cout << "Exiting Robot Fleet Management System." << endl;
-    return 0; // End of the program
 
     return 0; // End of the program
 }
