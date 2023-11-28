17897108d4637dce22c229fca1653729291686f3
Waleed Said <waleedsaid543@gmail.com>
2023-11-28 12:53:52 -0500

    remove add robot and add room options for user

--- app/fleet_manager.cpp
+++ app/fleet_manager.cpp
@@ -3,6 +3,8 @@
 #include <sstream>
 #include "../include/manage/Management.hpp" // Include Management Header File
 
+
+
 using namespace std;
 
 int main() {
@@ -21,45 +23,45 @@ int main() {
     // Main loop for program to run
     while (true) {
         // Just added the Perform Cleaning Operation
-        cout << "Please type a command (1: Add Robot, 2: Add Room, 3: Get Overall Room Status, 4: Get Overall Robot Status, 5: Perform Cleaning Operation, 6: Send Robot Home) or type 0 to Exit:";
+        cout << "Please type a command (1: Get Overall Room Status, 2: Get Overall Robot Status, 3: Perform Cleaning Operation, 4: Send Robot Home) or type 0 to Exit:";
         getline(cin, input); // Get the whole line as a command
 
         int command = stoi(input);
 
         // Check the user input and respond accordingly
         switch (command) {
+            // case 1: {
+            //     string robotId, robotStatus, robotSize, robotType;
+            //     cout << "Enter: [Robot ID] [robotStatus] [robotSize] [robotType] (in this order):" << endl;
+            //     getline(cin, input);
+            //     stringstream ss(input);
+            //     ss >> robotId >> robotStatus >> robotSize >> robotType;
+
+            //     management.add_new_robot(robotId, robotStatus, robotSize, robotType);
+            //     cout << "New Robot has been added!" << endl;
+            //     break;
+            // }
+            // case 2: {
+            //     string roomName, roomStatus, roomSize, estimatedTime;
+            //     cout << "Enter: [Room Name] [Room Status] [Room Size] [Estimated Time] (in this order):" << endl;
+            //     getline(cin, input);
+            //     stringstream ss(input);
+            //     ss >> roomName >> roomStatus >> roomSize >> estimatedTime;
+
+            //     management.add_new_room(roomName, roomStatus, roomSize, estimatedTime);
+            //     cout << "New Room has been added!" << endl;
+            //     break;
+            // }
             case 1: {
-                string robotId, robotStatus, robotSize, robotType;
-                cout << "Enter: [Robot ID] [robotStatus] [robotSize] [robotType] (in this order):" << endl;
-                getline(cin, input);
-                stringstream ss(input);
-                ss >> robotId >> robotStatus >> robotSize >> robotType;
-
-                management.add_new_robot(robotId, robotStatus, robotSize, robotType);
-                cout << "New Robot has been added!" << endl;
-                break;
-            }
-            case 2: {
-                string roomName, roomStatus, roomSize, estimatedTime;
-                cout << "Enter: [Room Name] [Room Status] [Room Size] [Estimated Time] (in this order):" << endl;
-                getline(cin, input);
-                stringstream ss(input);
-                ss >> roomName >> roomStatus >> roomSize >> estimatedTime;
-
-                management.add_new_room(roomName, roomStatus, roomSize, estimatedTime);
-                cout << "New Room has been added!" << endl;
-                break;
-            }
-            case 3: {
                 cout << management.to_string_room_list() << endl;
                 break;
             }
-            case 4: {
+            case 2: {
                 cout << management.to_string_robot_list() << endl;
                 break;
             }
             //Cleaning Operation
-            case 5: {
+            case 3: {
                 string robotId, roomId;
                 cout << "Enter Robot ID and Room ID for cleaning operation (separated by space):" << endl;
                 getline(cin, input);
@@ -71,7 +73,7 @@ int main() {
                 cout << "Cleaning operation initiated for Robot ID: " << robotId << " in Room ID: " << roomId << endl;
                 break;
             }
-            case 6: {
+            case 4: {
                 // Hypothetical implementation for sending a robot home
                 cout << "Selected robot should be sent home for charging. [Hypothetical Implementation]" << endl;
                 break;
