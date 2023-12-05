1c63850b0b91da12c1f0822da2b4771861d50337
KathrynMew <kathrynmew@gmail.com>
2023-12-05 17:10:55 -0500

    Organizing test cases and sections
    For easier debugging

--- tests/robot_class_test.cpp
+++ tests/robot_class_test.cpp
@@ -13,64 +13,78 @@ TEST_CASE("Independent Class: Robot") {
     }
 
     SECTION("Concentration: Robot Constructor") {
+        Robot bot("0", "Free", "Small", "Vac", "3");
+        REQUIRE(bot.to_string_status() == "Status:\tFree");
+        REQUIRE(bot.to_string_size() == "Size:\tSmall");
+        REQUIRE(bot.to_string_type() == "Type:\tVaccuum");
+        REQUIRE(bot.get_room() == "3");
+    }
+
+    SECTION("Concentration: Robot Constructor Edge Case") {
         Robot bot("0", "free", "small", "vac", "3");
-        REQUIRE(bot.to_string_status() == "Status:\tBusy");
+        REQUIRE(bot.to_string_status() == "Status:\tFree");
         REQUIRE(bot.to_string_size() == "Size:\tSmall");
         REQUIRE(bot.to_string_type() == "Type:\tVaccuum");
         REQUIRE(bot.get_room() == "3");
     }
 
     SECTION("Concentration: Robot Status") { 
-        // COMING SOON: needs to be updated
-        Robot free("NA", "Free", "NA", "NA", "NA");
-        Robot edgecase_free("NA", "free", "NA", "NA", "NA");
-        REQUIRE(free.to_string_status() == "Status:\tFree");
-        REQUIRE(edgecase_free.to_string_status() == "Status:\tFree");
+        SECTION("Normal Cases") {
+            Robot free("NA", "Free", "Small", "Mop", "NA");
+            Robot busy("NA", "Busy", "Small", "Mop", "NA");
+            REQUIRE(free.to_string_status() == "Status:\tFree");
+            REQUIRE(busy.to_string_status() == "Status:\tBusy");
+        }
 
-        Robot busy("NA", "Busy", "NA", "NA", "NA");
-        Robot edgecase_busy("NA", "busy", "NA", "NA", "NA");
-        REQUIRE(busy.to_string_status() == "Status:\tBusy");
-        REQUIRE(edgecase_busy.to_string_status() == "Status:\tBusy");
+        SECTION("Edge Cases") {
+            Robot edgecase_free("NA", "free", "Small", "Mop", "NA");
+            Robot edgecase_busy("NA", "busy", "Small", "Mop", "NA");
+            REQUIRE(edgecase_free.to_string_status() == "Status:\tFree");
+            REQUIRE(edgecase_busy.to_string_status() == "Status:\tBusy");
+        }
     }
     
     SECTION("Concentration: Robot Size") { 
-        // COMING SOON: needs to be updated
-        Robot small("NA", "NA", "Small", "NA", "NA");
-        Robot edgecase_small("NA", "NA", "small", "NA", "NA");
-        REQUIRE(small.to_string_size() == "Size:\tSmall");
-        REQUIRE(edgecase_small.to_string_size() == "Size:\tSmall");
+        SECTION("Normal Cases") {
+            Robot small("NA", "Free", "Small", "Mop", "NA");
+            Robot med("NA", "Free", "Medium", "Mop", "NA");
+            Robot large("NA", "Free", "Large", "Mop", "NA");
+            REQUIRE(small.to_string_size() == "Size:\tSmall");
+            REQUIRE(med.to_string_size() == "Size:\tMedium");
+            REQUIRE(large.to_string_size() == "Size:\tLarge");
+        }
 
-        Robot med("NA", "NA", "Medium", "NA", "NA");
-        Robot edgecase_med("NA", "NA", "medium", "NA", "NA");
-        REQUIRE(med.to_string_size() == "Size:\tMedium");
-        REQUIRE(edgecase_med.to_string_size() == "Size:\tMedium");
-
-        Robot large("NA", "NA", "Large", "NA", "NA");
-        Robot edgecase_large("NA", "NA", "large", "NA", "NA");
-        REQUIRE(large.to_string_size() == "Size:\tLarge");
-        REQUIRE(edgecase_large.to_string_size() == "Size:\tLarge");
+        SECTION("Edge Cases") {
+            Robot edgecase_small("NA", "Free", "small", "Mop", "NA");
+            Robot edgecase_med("NA", "Free", "medium", "Mop", "NA");
+            Robot edgecase_large("NA", "Free", "large", "Mop", "NA");
+            REQUIRE(edgecase_small.to_string_size() == "Size:\tSmall");
+            REQUIRE(edgecase_med.to_string_size() == "Size:\tMedium");
+            REQUIRE(edgecase_large.to_string_size() == "Size:\tLarge");
+        }
     }
 
     SECTION("Concentration: Robot Type") { 
-        // COMING SOON: needs to be updated
-        Robot mop("NA", "NA", "NA", "Mop", "NA");
-        Robot edgecase_mop("NA", "NA", "NA", "mop", "NA");
-        REQUIRE(mop.to_string_type() == "Type:\tMop");
-        REQUIRE(edgecase_mop.to_string_type() == "Type:\tMop");
+        SECTION("Normal Cases") {
+            Robot mop("NA", "Free", "Small", "Mop", "NA");
+            Robot vac("NA", "Free", "Small", "Vac", "NA");
+            Robot scrub("NA", "Free", "Small", "Scrub", "NA");
+            REQUIRE(mop.to_string_type() == "Type:\tMop");
+            REQUIRE(vac.to_string_type() == "Type:\tVaccuum");
+            REQUIRE(scrub.to_string_type() == "Type:\tScrub");
+        }
 
-        Robot vac("NA", "NA", "NA", "Vac", "NA");
-        Robot edgecase_vac("NA", "NA", "NA", "vac", "NA");
-        REQUIRE(vac.to_string_type() == "Type:\tVaccuum");
-        REQUIRE(edgecase_vac.to_string_type() == "Type:\tVaccuum");
-
-        Robot scrub("NA", "NA", "NA", "Scrub", "NA");
-        Robot edgecase_scrub("NA", "NA", "NA", "scrub", "NA");
-        REQUIRE(scrub.to_string_type() == "Type:\tScrub");
-        REQUIRE(edgecase_scrub.to_string_type() == "Type:\tScrub");
+        SECTION("Edge Cases") {
+            Robot edgecase_mop("NA", "Free", "Small", "mop", "NA");
+            Robot edgecase_vac("NA", "Free", "Small", "vac", "NA");
+            Robot edgecase_scrub("NA", "Free", "Small", "scrub", "NA");
+            REQUIRE(edgecase_mop.to_string_type() == "Type:\tMop");
+            REQUIRE(edgecase_vac.to_string_type() == "Type:\tVaccuum");
+            REQUIRE(edgecase_scrub.to_string_type() == "Type:\tScrub");
+        }
     }
 
     SECTION("Concentration: Robot Room Assignment") { 
-        // COMING SOON: needs to be updated
         Robot cleaner("0", "Free", "Small", "Mop", "NA");
         REQUIRE(cleaner.get_room() == "NA");
         cleaner.set_room("3");
--- tests/room_class_test.cpp
+++ tests/room_class_test.cpp
@@ -7,21 +7,78 @@
  * TESTING: Room Class
 */
 TEST_CASE("Independent Class: Room") {
-    Room room_passed_1("Conference", "dirty", "small", "5");
-    Room room_passed_2("Conference", "Dirty", "Small", "5");
-    std::string expected_room = 
-    "Room Name:\tConference\nRoom Status:\tDirty\nRoom Size:\tSmall\nEstimated Time to Clean: 5 minutes";
-    CHECK(room_passed_1.to_string() == expected_room);
-    CHECK(room_passed_2.to_string() == expected_room);
+    SECTION("Concentation: Room Default Constructor") {
+        Room myroom;
+        REQUIRE(myroom.to_string_status() == "Room Status:\tUnknown");
+        REQUIRE(myroom.to_string_size() == "Room Size:\tUnknown");
+        REQUIRE(myroom.get_time_to_clean() == 0);
+    }
 
+    SECTION("Concentration: Room Constructor") {
+        Room myroom("1", "Dirty", "Small", "12");
+        REQUIRE(myroom.to_string_status() == "Room Status:\tDirty");
+        REQUIRE(myroom.to_string_size() == "Room Size:\tSmall");
+        REQUIRE(myroom.get_time_to_clean() == 12);
+    }
+
+    SECTION("Concentration: Room Constructor Edge Case") {
+        Room myroom("1", "dirty", "small", "12");
+        REQUIRE(myroom.to_string_status() == "Room Status:\tDirty");
+        REQUIRE(myroom.to_string_size() == "Room Size:\tSmall");
+        REQUIRE(myroom.get_time_to_clean() == 12);
+    }
+    
+    SECTION("Concentration: Room Status") {
+        SECTION("Normal Cases") {
+            Room dirty("NA", "Dirty", "Small", "0");
+            Room inprogress("NA", "In-progress", "Small", "0");
+            Room clean("NA", "Clean", "Small", "0");
+            REQUIRE(dirty.to_string_status() == "Room Status:\tDirty");
+            REQUIRE(inprogress.to_string_status() == "Room Status:\tIn-progress");
+            REQUIRE(clean.to_string_status() == "Room Status:\tClean");
+        }
+
+        SECTION("Edge Cases") {
+            Room edgecase_dirty("NA", "dirty", "Small", "0");
+            Room edgecase_inprogress("NA", "in-progess", "Small", "0");
+            Room edgecase_inprogress2("NA", "in progess", "Small", "0");
+            Room edgecase_inprogress3("NA", "IP", "Small", "0");
+            Room edgecase_clean("NA", "clean", "Small", "0");
+            REQUIRE(edgecase_dirty.to_string_status() == "Room Status:\tDirty");
+            REQUIRE(edgecase_inprogress.to_string_status() == "Room Status:\tIn-progress");
+            REQUIRE(edgecase_inprogress2.to_string_status() == "Room Status:\tIn-progress");
+            REQUIRE(edgecase_inprogress3.to_string_status() == "Room Status:\tIn-progress");
+            REQUIRE(edgecase_clean.to_string_status() == "Room Status:\tClean");
+        }
+    }
+
+    SECTION("Concentration: Room") { 
+        SECTION("Normal Cases") {
+            Room small("NA", "Dirty", "Small", "0");
+            Room med("NA", "Dirty", "Medium", "0");
+            Room large("NA", "Dirty", "Large", "0");
+            REQUIRE(small.to_string_size() == "Size:\tSmall");
+            REQUIRE(med.to_string_size() == "Size:\tMedium");
+            REQUIRE(large.to_string_size() == "Size:\tLarge");
+        }
+
+        SECTION("Edge Cases") {
+            Room edgecase_small("NA", "Dirty", "small", "0");
+            Room edgecase_med("NA", "Dirty", "medium", "0");
+            Room edgecase_large("NA", "Dirty", "large", "0");
+            REQUIRE(edgecase_small.to_string_size() == "Size:\tSmall");
+            REQUIRE(edgecase_med.to_string_size() == "Size:\tMedium");
+            REQUIRE(edgecase_large.to_string_size() == "Size:\tLarge");
+        }
+    }
 
     SECTION("Concentration: Throw Error") {
         REQUIRE_THROWS_WITH(
-            Room("Conference", "sdkfjslfdkajlfkwjf", "small", "5"),
+            Room("0", "d", "small", "5"),
             Catch::Contains("Invalid argument: Received an invalid status")
         );
         REQUIRE_THROWS_WITH(
-            Room("Conference", "dirty", "sldfkjslefkejweflkj", "5"),
+            Room("0", "dirty", "x-large", "5"),
             Catch::Contains("Invalid argument: Received an invalid size")
         );
     } 
