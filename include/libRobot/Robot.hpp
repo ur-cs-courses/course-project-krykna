e4101ee6452eadcab2e5f2da2b59ea65e1bd1faf
KathrynMew <kathrynmew@gmail.com>
2023-12-05 11:19:54 -0500

    Standardized Robot class files
    Took out "using namespace std"

--- include/libRobot/Robot.hpp
+++ include/libRobot/Robot.hpp
@@ -2,7 +2,6 @@
 #define LIBROBOT_ROBOT_HPP
 #include <string>
 #include <stdexcept>
-using namespace std;
 
 enum Robot_Status {Free, Busy};
 enum Robot_Size {Small, Medium, Large};
@@ -10,41 +9,41 @@ enum Type {Mop, Vac, Scrub};
 
 class Robot {
     private:
-        string id_;
+        std::string id_;
         Robot_Size size_;
         Robot_Status status_;
         Type type_;
-        string room_id_;
+        std::string room_id_;
     
     public:
         // Constructors
         Robot();
 
-        Robot(string ID, string status_, string size_, string type_, string room_id_);
+        Robot(std::string ID, std::string status_, std::string size_, std::string type_, std::string room_id_);
 
         Robot(const Robot& other);
 
         // Destructor
         ~Robot();
 
-        void set_type(string& bot_type_);
-        void set_size(string& bot_size_);
+        void set_type(std::string& bot_type_);
+        void set_size(std::string& bot_size_);
 
         // Room
-        void set_room(string room);
-        inline string get_room() const {return this->room_id_;}
+        void set_room(std::string room);
+        inline std::string get_room() const {return this->room_id_;}
 
         // Status
         // NOTE: Would recommend avoiding any change to this as it is used by Robot Constructor
-        void set_status(string status);
+        void set_status(std::string status);
     
         // Robot activities
         void go_home();
 
-        string to_string_type();
-        string to_string_status();
-        string to_string_size();
-        string to_string();
+        std::string to_string_type();
+        std::string to_string_status();
+        std::string to_string_size();
+        std::string to_string();
 
         inline Robot_Size get_size() const {return this->size_;}
 
